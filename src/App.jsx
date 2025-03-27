11bd2a7af9e0965025ac0cc14aa0198968e1ddbc
garrettjbond <gbond.dev@gmail.com>
2025-03-26 21:09:11 -0600

    test comment

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    {/* Can you see me? - Garrett */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
